81603d7da01c3403e4ffd1f760740100693b2055
athibau2 <a.thibs98@gmail.com>
2023-05-11 02:17:46 -0600

    logged out env var for testing

--- store/db.js
+++ store/db.js
@@ -1,2 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 export const SUPABASE = createClient('https://ypcpzttjynqtrafqfjir.supabase.co', process.env.SUPABASE_KEY)
+console.log(process.env.SUPABASE_KEY)
