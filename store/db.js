6010c76946b279cd1f67758949f38296c20f7be3
Andrew <a.thibs98@gmail.com>
2025-04-25 14:50:16 -0600

    updated supabase values with new project

--- store/db.js
+++ store/db.js
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRscmNtc2lldHd1cXp2ZHlicWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkwNDA4NjUsImV4cCI6MjAxNDYxNjg2NX0.eCs600a6_XmNEzrqfGoge_r1EioCy4fpRlhVtP9qBfM'
-export const SUPABASE = createClient('https://dlrcmsietwuqzvdybqdr.supabase.co', SUPABASE_KEY)
+const SUPABASE_KEY = process.env.NUXT_ENV_SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.NUXT_ENV_SUPABASE_URL;
+export const SUPABASE = createClient(SUPABASE_URL, SUPABASE_KEY);
